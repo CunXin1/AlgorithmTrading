4b3bb55b5911a95ffbfd9e267dc7838fc32705c6
Ruibo's Desktop <sunruibo5@gmail.com>
2025-11-06 17:44:59 -0800

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', //确保部署在根路径时路由和资源能正常加载
 })
