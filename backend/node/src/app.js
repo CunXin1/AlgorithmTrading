718b3ae8a74b90cb7ab46213238cbab5299539ca
Ruibo's Desktop <sunruibo5@gmail.com>
2025-12-11 01:09:59 -0800

    update

--- backend/node/src/app.js
+++ backend/node/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./modules/auth/authRoutes.js";
 import stockRoutes from "./modules/stocks/stockRoutes.js";
+import cors from "cors";
 
 const app = express();
 
@@ -11,3 +12,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/stocks", stockRoutes);
 
 export default app;
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: false
+}));
