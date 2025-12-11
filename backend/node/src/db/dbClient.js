db83f304f90faf7934d05fcb63cd75d42dc009a6
Ruibo's Desktop <sunruibo5@gmail.com>
2025-12-11 00:39:55 -0800

    update

--- backend/node/src/db/dbClient.js
+++ backend/node/src/db/dbClient.js
@@ -1,12 +1,5 @@
-import "dotenv/config";
-import { PrismaClient } from "./generated/prisma/client/index.js";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-// Prisma 7 要求使用 adapter 或 accelerateUrl，这里用 pg adapter
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const db = new PrismaClient({ adapter });
+const db = new PrismaClient();
 
 export default db;
