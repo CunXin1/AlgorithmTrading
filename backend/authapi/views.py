d0a7bd534ef66c0a1c9e5356568b45b764bc9b57
Ruibo's Desktop <sunruibo5@gmail.com>
2025-12-16 00:53:14 -0800

    update

--- backend/authapi/views.py
+++ backend/authapi/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.conf import settings
 
+from threading import Thread
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
